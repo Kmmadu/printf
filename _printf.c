c72a82a0ef6e1edd6c9b5ccf72460a0439fc57b1
ludenyi <86718951+ludenyi@users.noreply.github.com>
2023-10-16 22:13:30 +0300

    I just checked the code

--- _printf.c
+++ _printf.c
@@ -5,6 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
+ * seen the code 
  * Return: Printed chars.
  */
 
@@ -55,6 +56,7 @@ return (printed_chars);
 /**
 * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
+ * 
  * @buff_ind: Index at which to add next char, represents the length.
  */
 
