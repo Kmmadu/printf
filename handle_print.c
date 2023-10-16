adc9bda695e802943883b169fb5138c988623054
ludenyi <86718951+ludenyi@users.noreply.github.com>
2023-10-16 23:07:03 +0300

    task 2

--- handle_print.c
+++ handle_print.c
@@ -2,6 +2,7 @@
 /**
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
+ * cd change directory
  * @list: List of arguments to be printed.
  * @ind: ind.
  * @buffer: Buffer array to handle print.
